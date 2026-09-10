a43301f0190af5356c932f002bd185fe6f2bbe91
Long Ma <73346969+77malong@users.noreply.github.com>
2026-09-10 14:29:02 +0800

    refactor(models): make adapter registration order explicit

--- ser_lib/models/__init__.py
+++ ser_lib/models/__init__.py
@@ -1,8 +1,8 @@
-from ser_lib.models.adapters import TORCH_ADAPTER_MODEL_ID, TorchModelAdapter
 from ser_lib.models.base import ModelOutput, SERModel
-from ser_lib.models.cnn_models import CNNBaseline, CNNBaselineConfig
-from ser_lib.models.pretrained import HFAudioClassifier, HFAudioClassifierConfig
 from ser_lib.models.registry import ModelDescriptor, ModelRegistry, model_registry
+from ser_lib.models.adapters import TORCH_ADAPTER_MODEL_ID, TorchModelAdapter
+from ser_lib.models.cnn_models import CNNBaseline, CNNBaselineConfig
+from ser_lib.pretrained import HFAudioClassifier, HFAudioClassifierConfig
 from ser_lib.models.rnn_models import GRUBaseline, GRUBaselineConfig
 from ser_lib.models.specs import ModelSpec
 from ser_lib.models.transformer_models import TransformerBaseline, TransformerBaselineConfig
