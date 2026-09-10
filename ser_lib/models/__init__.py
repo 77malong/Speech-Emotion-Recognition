18054e1680200d6eb5a797f0a9d52a972b33b89c
Long Ma <73346969+77malong@users.noreply.github.com>
2026-09-10 14:23:22 +0800

    feat(models): expose torch model adapter

--- ser_lib/models/__init__.py
+++ ser_lib/models/__init__.py
@@ -1,14 +1,27 @@
+from ser_lib.models.adapters import TORCH_ADAPTER_MODEL_ID, TorchModelAdapter
 from ser_lib.models.base import ModelOutput, SERModel
 from ser_lib.models.cnn_models import CNNBaseline, CNNBaselineConfig
-from ser_lib.models.rnn_models import GRUBaseline, GRUBaselineConfig
-from ser_lib.models.transformer_models import TransformerBaseline, TransformerBaselineConfig
 from ser_lib.models.pretrained import HFAudioClassifier, HFAudioClassifierConfig
 from ser_lib.models.registry import ModelDescriptor, ModelRegistry, model_registry
+from ser_lib.models.rnn_models import GRUBaseline, GRUBaselineConfig
 from ser_lib.models.specs import ModelSpec
+from ser_lib.models.transformer_models import TransformerBaseline, TransformerBaselineConfig
+
 __all__ = [
-    "SERModel", "ModelOutput", "ModelSpec",
-    "CNNBaseline", "CNNBaselineConfig", "GRUBaseline", "GRUBaselineConfig",
-    "TransformerBaseline", "TransformerBaselineConfig",
-    "HFAudioClassifier", "HFAudioClassifierConfig",
-    "ModelDescriptor", "ModelRegistry", "model_registry",
+    "SERModel",
+    "ModelOutput",
+    "ModelSpec",
+    "TorchModelAdapter",
+    "TORCH_ADAPTER_MODEL_ID",
+    "CNNBaseline",
+    "CNNBaselineConfig",
+    "GRUBaseline",
+    "GRUBaselineConfig",
+    "TransformerBaseline",
+    "TransformerBaselineConfig",
+    "HFAudioClassifier",
+    "HFAudioClassifierConfig",
+    "ModelDescriptor",
+    "ModelRegistry",
+    "model_registry",
 ]
